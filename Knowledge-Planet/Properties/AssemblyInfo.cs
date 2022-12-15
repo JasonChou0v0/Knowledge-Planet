b9afece2357fa54f602978da4abb0b5fe3fd7388
zhouzhou <zhouzhou.chn@gmail.com>
2022-12-15 19:17:28 +0800

    重置项目结构

--- Knowledge-Planet/Properties/AssemblyInfo.cs
+++ Knowledge-Planet/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于 typelib 的 ID
-[assembly: Guid("5c18655b-15f0-4fa1-9078-9c219c69e6b6")]
+[assembly: Guid("efb20b96-6cb6-45ff-bbe1-0cfe95aa6359")]
 
 // 程序集的版本信息由下列四个值组成:
 //
